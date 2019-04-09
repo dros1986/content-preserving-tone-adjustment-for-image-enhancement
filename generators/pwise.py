ec2c8aa9492d4368523f3c498246417e2b4f47d3
Flavio Piccoli <flavio.piccoli@disco.unimib.it>
2019-04-09 14:02:50 +0200

    Forgot some updates.

--- generators/pwise.py
+++ generators/pwise.py
@@ -8,30 +8,31 @@ import random
 class Piecewise(torch.nn.Module):
     def __init__(self, params):
         super(Piecewise,self).__init__()
-        self.params = params
-        self.emodule = PiecewiseBasis(params['n'])
-        self.c1 = torch.nn.Conv2d(3, 8, kernel_size=5, stride=4, padding=0)
-        self.r1 = torch.nn.PReLU(num_parameters=8, init=0.25)
-        self.c2 = torch.nn.Conv2d(8, 16, kernel_size=3, stride=2, padding=0)
-        self.r2 = torch.nn.PReLU(num_parameters=16, init=0.25)
-        self.c3 = torch.nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=0)
-        self.r3 = torch.nn.PReLU(num_parameters=32, init=0.25)
-        self.c4 = torch.nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=0)
-        self.r4 = torch.nn.PReLU(num_parameters=64, init=0.25)
-        self.downsample = torch.nn.AvgPool2d(7, stride=1)
+        # define number of filters
+        pow = params['fpow'] if 'fpow' in params else 4
+        nf = 2**pow
+        # define enhancement module
+        basis_param = params['n']
+        self.emodule = PiecewiseBasis(basis_param)
+        # define network
+        self.c2 = torch.nn.Conv2d(   3,   nf, kernel_size=3, stride=2, padding=0)
+        self.r2 = torch.nn.PReLU(num_parameters=  nf, init=0.25)
+        self.c3 = torch.nn.Conv2d(  nf, 2*nf, kernel_size=3, stride=2, padding=0)
+        self.r3 = torch.nn.PReLU(num_parameters=2*nf, init=0.25)
+        self.c4 = torch.nn.Conv2d(2*nf, 4*nf, kernel_size=3, stride=2, padding=0)
+        self.r4 = torch.nn.PReLU(num_parameters=4*nf, init=0.25)
+        self.downsample = torch.nn.AvgPool2d(11, stride=1)
         self.fc = torch.nn.Sequential(
             torch.nn.Linear(64, 64),
             torch.nn.PReLU(num_parameters=64, init=0.25),
             torch.nn.Linear(64, self.emodule.parameters_count)
         )
 
-
     def forward(self, image, applyto=None):
         x = image
-        if (image.size(2), image.size(3)) != (256, 256):
-            x = _bilinear(x, 256, 256)
+        if (image.size(2), image.size(3)) != (100, 100):
+            x = _bilinear(x, 100, 100)
         x = x - 0.5
-        x = self.r1(self.c1(x))
         x = self.r2(self.c2(x))
         x = self.r3(self.c3(x))
         x = self.r4(self.c4(x))
@@ -87,3 +88,17 @@ def _bilinear(im, height, width):
     grid = torch.stack(mesh[::-1], 2).unsqueeze(0)
     grid = grid.expand(im.size(0), height, width, 2)
     return F.grid_sample(im, grid)
+
+
+
+
+if __name__ == "__main__":
+    params = {
+        "n":10
+    }
+    net = Piecewise(params)
+    import ipdb; ipdb.set_trace()
+    print(net)
+    x = torch.rand(7, 3, 100, 100)
+    y = net(x)
+    print(y.size())
