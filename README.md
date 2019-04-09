9305cbe0b99c1284f9df56cd84ac239eaf192a00
Flavio Piccoli <flavio.piccoli@disco.unimib.it>
2019-04-09 13:55:55 +0200

    Updated to best solution.

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ## Inference
 
-1 - Change the directory that must be enhanced in the file: "_./json/enhancement\_pwise\_blurred\_n\_10\_lr\_1e-4\_wd\_0.0.json_" at the key "_dataset_ &rightarrow; _params_ &rightarrow; _regen_ &rightarrow; _input_dir_"
+1 - Change the directory that must be enhanced in the file: "_enh\_pwise\_single\_filt\_7\_wd\_0.0_" at the key "_dataset_ &rightarrow; _params_ &rightarrow; _regen_ &rightarrow; _input_dir_"
 
 2 - Run the code with the command: 
 
 ```sh
-python Trainer.py --json ./json/enhancement_pwise_blurred_n_10_lr_1e-4_wd_0.0.json --regen
+python Trainer.py --json ./json/enh_pwise_single_filt_7_wd_0.0.json --regen
 ```
 
-3 - Enhanced images will be in the folder: "_./checkpoints/enhancement_pwise_blurred_n_10_lr_1e-4_wd_0.0/regen_"
+3 - Enhanced images will be in the folder: "_./checkpoints/enh_pwise_single_filt_7_wd_0.0/regen_"
--- generators/filter_estimator.py
+++ generators/filter_estimator.py
@@ -2,22 +2,56 @@ import math
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
+import scipy.stats as st
 import torch.nn.functional as F
+
 
 
 class FilterEstimator(nn.Module):
 	def __init__(self, params):
 		super(FilterEstimator, self).__init__()
 		self.params = params
-		# define down and up block
-		self.filter = self.get_filter(params['size'])
+		# define filter type
+		self.ftype = self.params['filter_type'] if 'filter_type' in self.params else 'rgb'
+		# define filter parameters
+		if self.ftype == 'rgb':
+			# create 4D filter
+			kernel = self.gaussian_filter()
+			new_f = torch.zeros(3,3,kernel.size(0),kernel.size(1))
+			for i in range(3):
+				new_f[i,i,:,:] = kernel
+			# save parameter
+			self.filter = torch.nn.Parameter(new_f)
+		else:
+			self.filter = torch.nn.Parameter(self.gaussian_filter())
+		# create padding layer
+		self.pad = nn.ReflectionPad2d(int((self.params['size']-1)/2))
+
 
 	def forward(self,x):
-		return self.filter(x)
+		# pad input
+		x = self.pad(x)
+		# convolve with filter
+		if self.ftype == 'rgb':
+			x = F.conv2d(x, self.filter)
+		else:
+			new_f = torch.zeros(3,3,self.filter.size(0),self.filter.size(1)).to(x.device)
+			for i in range(3): new_f[i,i,:,:] = self.filter
+			x = F.conv2d(x, new_f)
+		# return image
+		return x
 
-	def get_filter(self,sz):
-		padsz = int((sz-1)/2)
-		return nn.Sequential(
-			nn.ReflectionPad2d(padsz),
-			nn.Conv2d(3, 3, kernel_size=sz, stride=1, padding=0)
-		)
+
+	def gaussian_filter(self):
+		# create a 2D gaussian kernel array
+		kernlen = self.params['size']
+		nsig = self.params['sigma']
+		interval = (2*nsig+1.)/(kernlen)
+		x = np.linspace(-nsig-interval/2., nsig+interval/2., kernlen+1)
+		kern1d = np.diff(st.norm.cdf(x))
+		kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
+		kernel = kernel_raw/kernel_raw.sum()
+		# convert to torch
+		kernel = torch.from_numpy(kernel)
+		return kernel
